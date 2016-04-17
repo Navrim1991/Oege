3ed314d257b6460a94597f6b45e9e2e1fc4fad35
mk <marvinkloster@web.de>
2016-04-18 00:30:54 +0200

    MVC

---
+++ Oege_Get_the_best_price/Oege_Get_the_best_price/Controller/Excel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oege_Get_the_best_price.Controller.Excel
+{
+    class Excel
+    {
+    }
+}
